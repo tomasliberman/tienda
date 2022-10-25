04e4a9ab9448ea528f988eb3894288e25af18049
Tomás Liberman <tomasliberman2004@gmail.com>
2022-10-25 19:38:07 -0300

    javascript

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//sumar dos numeros ingresados
+
+const numero1= parseInt(prompt("ingrese #1"));
+const numero2= parseInt(prompt("ingrese #2"));
+
+alert(numero1 + numero2)
+
+//averiguar resto de division
+alert(numero1 % numero2)
+
+
+//condicion
+
+let num = parseInt(prompt("ingrese tu numero")) 
+
+if (num == 10) {
+    alert ("el numero es correcto");
+}else {
+    alert ("el numero es incorrecto")
+}
+
+// el doble == es una comparación
