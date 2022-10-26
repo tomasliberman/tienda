0b59633449c300b396eb148a00b8ccc44e897bf8
Tomás Liberman <tomasliberman2004@gmail.com>
2022-10-25 21:44:37 -0300

    javascript

--- js/main.js
+++ js/main.js
@@ -1,22 +1,45 @@
-//sumar dos numeros ingresados
+let prenda = prompt("Elija su prenda por su numero \n 1) remera \n 2) campera \n 3) zapatillas \n 4) medias \n 5) gorras");
 
-const numero1= parseInt(prompt("ingrese #1"));
-const numero2= parseInt(prompt("ingrese #2"));
+let presupuesto = parseInt(prompt("ingrese cuannto dinero tiene"));
+const remera = 2000;
+const campera = 4000;
+const zapatillas = 3500;
+const medias = 500;
+const gorras = 1000;
 
-alert(numero1 + numero2)
+let sobra = presupuesto-remera;
 
-//averiguar resto de division
-alert(numero1 % numero2)
+if (presupuesto-remera >= 0){
+    alert("puedes comprar el producto"); 
+    alert("sobran $"+sobra);
 
+} else if (presupuesto-campera >= 0){
+    alert("puedes comprar el producto");
+    alert("sobran $"+sobra); 
 
-//condicion
+}else if (presupuesto-zapatillas >= 0){
+    alert("puedes comprar el producto"); 
+    alert("sobran $"+sobra);
 
-let num = parseInt(prompt("ingrese tu numero")) 
+}else if (presupuesto-medias >= 0){
+    alert("puedes comprar el producto");
+    alert("sobran $"+sobra); 
 
-if (num == 10) {
-    alert ("el numero es correcto");
+}else if (presupuesto-gorras >= 0){
+    alert("puedes comprar el producto"); 
+    alert("sobran $"+sobra);
+    
+
 }else {
-    alert ("el numero es incorrecto")
+    alert("su dinero no es suficiente")
+}; 
+
+for (let i = 0; presupuesto < 500; i--) {
+    if (i < 0) {
+        alert ("usted no puede comprar ningun producto");
+        break;
+    } 
 }
 
-// el doble == es una comparación
+
+
